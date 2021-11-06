c3292ccc3be3d8ebcade64c2131f386268be472d
Michael Agbaje <seyimichael16@gmail.com>
2021-11-06 23:10:34 +0800

    .

--- lib/screens/body/home_form.dart
+++ lib/screens/body/home_form.dart
@@ -3,65 +3,128 @@ import 'package:book_app/component/components/discount_banner.dart';
 import 'package:book_app/component/special_offers_card.dart';
 import 'package:book_app/constants.dart';
 import 'package:book_app/models/product.dart';
-// import 'package:book_app/component/components/search_field.dart';
-// import 'package:book_app/constants.dart';
+
 import 'package:book_app/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
 
 import 'home_header.dart';
-// import '../component/components/icon_btn.dart';/
 
 class HomeForm extends StatelessWidget {
   const HomeForm({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            height: getProportionateScreenWidth(20),
-          ),
-          HomeHeader(),
-          SizedBox(
-            height: getProportionateScreenWidth(30),
-          ),
-          DiscountBanner(),
-          SizedBox(
-            height: getProportionateScreenWidth(30),
-          ),
-          Categories(),
-          SizedBox(
-            height: getProportionateScreenWidth(30),
-          ),
-          SpecialOffersCard(),
-          SizedBox(
-            height: getProportionateScreenWidth(30),
-          ),
-          SectionTitle(
-            text: 'Popular Product',
-            press: () {},
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: getProportionateScreenWidth(140),
-                child: AspectRatio(
-                  aspectRatio: 1.02,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(15)
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: getProportionateScreenWidth(20),
+            ),
+            HomeHeader(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            DiscountBanner(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            Categories(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            SpecialOffersCard(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            SectionTitle(
+              text: 'Popular Product',
+              press: () {},
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ...List.generate(
+                    demoProducts.length,
+                    (index) => ProductCard(
+                      product: demoProducts[index],
                     ),
-                    child: Image.asset(demoProducts[0].images[0]),
                   ),
+                  ProductCard(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+    this.width = 120,
+    this.aspectRetion = 1.02,
+    this.product,
+  }) : super(key: key);
+
+  final double width, aspectRetion;
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+      child: SizedBox(
+        width: getProportionateScreenWidth(width),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: aspectRetion,
+              child: Container(
+                padding: EdgeInsets.all(
+                  getProportionateScreenWidth(20),
                 ),
-              )
-            ],
-          )
-        ],
+                decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Image.asset(product.images[0]),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              demoProducts[0].title,
+              style: TextStyle(color: Colors.black),
+              maxLines: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '\$${demoProducts[0].price}',
+                  style: TextStyle(
+                      fontSize: getProportionateScreenWidth(18),
+                      fontWeight: FontWeight.w600,
+                      color: kPrimaryColor),
+                ),
+                Container(
+                  padding: EdgeInsets.all(
+                    getProportionateScreenWidth(5),
+                  ),
+                  width: getProportionateScreenWidth(20),
+                  height: getProportionateScreenWidth(20),
+                  decoration: BoxDecoration(
+                      color: kSecondaryColor.withOpacity(0.1),
+                      shape: BoxShape.circle),
+                  child: SvgPicture.asset('assets/icons/Heart Icon_2.svg'),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
