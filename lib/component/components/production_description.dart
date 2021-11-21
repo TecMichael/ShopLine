fe701142f6bbb553e488f8080bb056234545c74e
Michael Agbaje <seyimichael16@gmail.com>
2021-11-21 20:27:20 +0800

    product

---
+++ lib/component/components/production_description.dart
@@ -0,0 +1,90 @@
+import 'package:book_app/models/product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../constants.dart';
+import '../../size_config.dart';
+
+class ProductionDescription extends StatelessWidget {
+  const ProductionDescription({
+    Key key,
+    @required this.product,
+    this.pressOnSeeMore,
+  }) : super(key: key);
+
+  final Product product;
+  final GestureTapCallback pressOnSeeMore;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20),
+          ),
+          child: Text(
+            product.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        const SizedBox(height: 5),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            padding: EdgeInsets.all(
+              getProportionateScreenWidth(15),
+            ),
+            width: getProportionateScreenWidth(64),
+            decoration: BoxDecoration(
+              color:
+                  product.isFavourite ? Color(0xffffe6e6) : Color(0xfff5f6f9),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/Heart Icon_2.svg',
+              color:
+                  product.isFavourite ? Color(0xffff4848) : Color(0xffdbdee4),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: getProportionateScreenWidth(20),
+            right: getProportionateScreenWidth(64),
+          ),
+          child: Text(
+            product.description,
+            maxLines: 3,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20), vertical: 10),
+          child: GestureDetector(
+            onTap: pressOnSeeMore,
+            child: Row(
+              children: [
+                Text(
+                  'See More Details',
+                  style: TextStyle(
+                      color: kPrimaryColor, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(width: 5),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 12,
+                  color: kPrimaryColor,
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/screens/details/details.form.dart
+++ lib/screens/details/details.form.dart
@@ -1,4 +1,9 @@
+import 'package:book_app/component/components/color_dots.dart';
+import 'package:book_app/component/components/default_button.dart';
 import 'package:book_app/component/components/product_images.dart';
+import 'package:book_app/component/components/production_description.dart';
+import 'package:book_app/component/components/rounded_icon_btn.dart';
+import 'package:book_app/component/components/top_rounded_container.dart';
 import 'package:book_app/models/product.dart';
 import 'package:book_app/size_config.dart';
 import 'package:flutter/material.dart';
@@ -10,58 +15,95 @@ class DetailsForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ProductImages(product: product),
-        TopRoundedContainer(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(20)),
-                child: Text(
-                  product.title,
-                  style: Theme.of(context).textTheme.headline6,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ProductImages(product: product),
+          TopRoundedContainer(
+            color: Colors.white,
+            child: Column(
+              children: [
+                ProductionDescription(
+                  product: product,
+                  pressOnSeeMore: () {},
                 ),
-              )
-            ],
+                TopRoundedContainer(
+                  color: Color(0xfff6f7f9),
+                  child: Column(
+                    children: [
+                      ColorDots(product: product),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: SizeConfig.screenWidth * 0.15,
+                          right: SizeConfig.screenWidth * 0.15,
+                          top: getProportionateScreenWidth(15),
+                          bottom: getProportionateScreenWidth(40),
+                        ),
+                        child: TopRoundedContainer(
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: SizeConfig.screenWidth * 0.11,
+                                right: SizeConfig.screenWidth * 0.11,
+                                bottom: getProportionateScreenWidth(40),
+                                top: getProportionateScreenWidth(15)),
+                            child: DefaultButton(
+                              text: 'Add to Cart',
+                              press: () {},
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
 
-class TopRoundedContainer extends StatelessWidget {
-  const TopRoundedContainer({
+class ColorDots extends StatelessWidget {
+  const ColorDots({
     Key key,
-    this.color,
-    this.child,
+    @required this.product,
   }) : super(key: key);
 
-  final Color color;
-  final Widget child;
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: getProportionateScreenWidth(20),
+    int selectedColor = 3;
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(20),
       ),
-      padding: EdgeInsets.only(
-        top: getProportionateScreenWidth(20),
+      child: Row(
+        children: [
+          ...List.generate(
+            product.colors.length,
+            (index) => ColorDot(
+              color: product.colors[index],
+              isSelected: selectedColor == index,
+            ),
+          ),
+          Spacer(),
+          RoundedIconBtn(
+            iconData: Icons.remove,
+            press: () {},
+          ),
+          SizedBox(
+            width: getProportionateScreenWidth(15),
+          ),
+          RoundedIconBtn(
+            iconData: Icons.add,
+            press: () {},
+          )
+        ],
       ),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40),
-        ),
-      ),
-      child: child,
     );
   }
 }
