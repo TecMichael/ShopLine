d7e9db5b9a09fd8197d615b515fc50a13ccde7cb
Michael Agbaje <seyimichael16@gmail.com>
2022-01-14 18:50:03 +0100

    upd

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,34 +1,49 @@
 import 'package:book_app/models/Cart.dart';
+import 'package:book_app/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'cart_item_card.dart';
 
-class CartBody extends StatelessWidget {
+class CartBody extends StatefulWidget {
   const CartBody({Key key}) : super(key: key);
 
   @override
+  State<CartBody> createState() => _CartBodyState();
+}
+
+class _CartBodyState extends State<CartBody> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Dismissible(
-          key: Key(demoCarts[0].product.id.toString()),
-          background: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              color: Color(0xffffe6e6),
-              borderRadius: BorderRadius.circular(15)
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+            key: Key(demoCarts[index].product.id.toString()),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Color(0xffffe6e6),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset('assets/icons/Trash.svg')
+                ],
+              ),
             ),
-            child: Row(
-              children: [
-                Spacer(),
-                SvgPicture.asset('assets/icons/Trash.svg')
-              ],
-            ),
+            onDismissed: (direction) {
+              demoCarts.removeAt(index);
+            },
+            child: CartItemCard(cart: demoCarts[index]),
           ),
-          child: CartItemCard(cart: demoCarts[0]),
         ),
-      ],
+      ),
     );
   }
 }
